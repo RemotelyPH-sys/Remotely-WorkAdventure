6aec377f162f01a062502664a3a2c908b6d345be
RemotelyPH-sys <remotely.temp@gmail.com>
2026-09-16 02:29:58 +0800

    update

---
+++ outdoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="outdoor" tilewidth="32" tileheight="32" tilecount="999" columns="37">
+ <image source="C:/Users/Admin/Downloads/tileset asset 1.png" width="1189" height="880"/>
+</tileset>
---
+++ tileset 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="tileset 1" tilewidth="32" tileheight="32" tilecount="999" columns="37">
+ <image source="tileset asset 1.png" width="1189" height="880"/>
+</tileset>
