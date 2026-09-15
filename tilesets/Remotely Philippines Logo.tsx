ed00d76f863a86d3e2f43d14255b8e935a945214
RemotelyPH-sys <remotely.temp@gmail.com>
2026-09-16 02:39:00 +0800

    update

---
+++ tilesets/Remotely Philippines Logo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Remotely Philippines Logo" tilewidth="32" tileheight="32" tilecount="363" columns="33">
+ <image source="RemotelyLogo.png" width="1081" height="361"/>
+</tileset>
